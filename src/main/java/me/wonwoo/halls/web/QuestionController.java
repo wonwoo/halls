2d84fa3c6b7d4b2fa357e84c3ad81a6cc739ae82
wonwoo <aoruqjfu@gmail.com>
2017-11-27 23:06:11 +0900

    docker 및 뷰 수정

--- src/main/java/me/wonwoo/halls/web/QuestionController.java
+++ src/main/java/me/wonwoo/halls/web/QuestionController.java
@@ -1,13 +1,12 @@
 package me.wonwoo.halls.web;
 
+import me.wonwoo.halls.domain.Question;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.SampleOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.result.view.Rendering;
-
-import me.wonwoo.halls.domain.Question;
 
 //@RestController
 @Controller
@@ -34,15 +33,15 @@ public class QuestionController {
 
     @GetMapping("/")
     public Rendering getQuestion() {
-        SampleOperation matchStage = Aggregation.sample(5);
+        SampleOperation matchStage = Aggregation.sample(1);
         Aggregation aggregation = Aggregation.newAggregation(matchStage);
         return Rendering.view("index")
                 .modelAttribute("questions", reactiveMongoTemplate.aggregate(aggregation, "question", Question.class)
-                        .map(question -> new QuestionDto(
-                                question.getQuestion(),
-                                question.getAnswer(),
-                                question.random(),
-                                question.answerIndex())))
+                        .map(q -> new QuestionDto(
+                                q.getQuestion(),
+                                q.getAnswer(),
+                                q.random(),
+                                q.answerIndex())).next())
                 .build();
     }
 }
